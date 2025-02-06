b68e58dd6d2de99caf85c990d17f373b57d56b0b
mecastor <mecastor87@gmail.com>
2025-02-06 08:56:53 +0000

    SignIn wip

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,78 +1,28 @@
-// import { SafeAreaView } from "react-native-safe-area-context";
-// import {
-//   Alert,
-//   Image,
-//   ScrollView,
-//   Text,
-//   TouchableOpacity,
-//   View,
-// } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, ScrollView, Text, View } from "react-native";
 
-// import { Redirect } from "expo-router";
-// import { useGlobalContext } from "@/lib/global-provider";
-// import icons from "@/constants/icons";
-// import images from "@/constants/images";
+import images from "@/constants/images";
 
-// const Auth = () => {
-//   const { refetch, loading, isLogged } = useGlobalContext();
+const SignIn = () => {
+  return (
+    <SafeAreaView className="h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.opusOrchestra}
+          className="w-full h-2/3"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-xl text-center uppercase font-rubik-bold">
+            Welcome to <Text className="text-orange-400">Opus</Text>
+          </Text>
+          <Text className="text-2xl text-center text-black font-rubik">
+            Make Finances {"\n"} Your Masterpiece!
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-//   if (!loading && isLogged) return <Redirect href="/" />;
-
-//   const handleLogin = async () => {
-//     const result = await login();
-//     if (result) {
-//       refetch();
-//     } else {
-//       Alert.alert("Error", "Failed to login");
-//     }
-//   };
-
-//   return (
-//     <SafeAreaView className="bg-white h-full">
-//       <ScrollView
-//         contentContainerStyle={{
-//           height: "100%",
-//         }}
-//       >
-//         <Image
-//           source={images.onboarding}
-//           className="w-full h-4/6"
-//           resizeMode="contain"
-//         />
-
-//         <View className="px-10">
-//           <Text className="text-base text-center uppercase font-rubik text-black-200">
-//             Welcome To Real Scout
-//           </Text>
-
-//           <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
-//             Let's Get You Closer To {"\n"}
-//             <Text className="text-primary-300">Your Ideal Home</Text>
-//           </Text>
-
-//           <Text className="text-lg font-rubik text-black-200 text-center mt-12">
-//             Login to Real Scout with Google
-//           </Text>
-
-//           <TouchableOpacity
-//             onPress={handleLogin}
-//             className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
-//           >
-//             <View className="flex flex-row items-center justify-center">
-//               <Image
-//                 source={icons.google}
-//                 className="w-5 h-5"
-//                 resizeMode="contain"
-//               />
-//               <Text className="text-lg font-rubik-medium text-black-300 ml-2">
-//                 Continue with Google
-//               </Text>
-//             </View>
-//           </TouchableOpacity>
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
-
-// export default Auth;
+export default SignIn;
--- constants/images.ts
+++ constants/images.ts
@@ -1,3 +1,4 @@
 import opusLogoOrange from "@/assets/images/opus-logo-orange.png";
+import opusOrchestra from "@/assets/images/opus-orchestra.png";
 
-export default opusLogoOrange;
+export default { opusLogoOrange, opusOrchestra };
