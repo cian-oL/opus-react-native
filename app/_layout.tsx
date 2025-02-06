69a8889016d88fb5764ebe4d63d88e8f290e574b
cian-oL <cianoleary2010@gmail.com>
2025-02-06 19:08:14 +0000

    Added Clerk SDK with root layout configuration and error handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,29 @@
-import { SplashScreen, Stack } from "expo-router";
-import { Text } from "react-native";
-
-import "./global.css";
+import { SplashScreen, Stack, useRouter } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+
+import "./global.css";
+import { tokenCache } from "@/cache";
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!CLERK_PUBLISHABLE_KEY) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
 
 // Add this line to prevent auto-hide
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
+const InitialLayout = () => {
+  const [fontsLoaded, error] = useFonts({
     "Rubik-Bold": require("@/assets/fonts/Rubik-Bold.ttf"),
     "Rubik-ExtraBold": require("@/assets/fonts/Rubik-ExtraBold.ttf"),
     "Rubik-Light": require("@/assets/fonts/Rubik-Light.ttf"),
@@ -17,6 +31,12 @@ export default function RootLayout() {
     "Rubik-Regular": require("@/assets/fonts/Rubik-Regular.ttf"),
     "Rubik-SemiBold": require("@/assets/fonts/Rubik-SemiBold.ttf"),
   });
+  const router = useRouter();
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
 
   useEffect(() => {
     if (fontsLoaded) {
@@ -29,4 +49,19 @@ export default function RootLayout() {
   }
 
   return <Stack screenOptions={{ headerShown: false }} />;
-}
+};
+
+const RootLayoutNav = () => {
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayoutNav;
