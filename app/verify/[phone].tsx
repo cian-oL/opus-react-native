3e8b65e34061a982ec6399e067c9735755a1f148
mecastor <mecastor87@gmail.com>
2025-02-06 22:37:28 +0000

    verification wup

--- app/verify/[phone].tsx
+++ app/verify/[phone].tsx
@@ -1,12 +1,31 @@
 import { View, Text } from "react-native";
-import React from "react";
+import { useEffect, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
+import { useSignIn, useSignUp } from "@clerk/clerk-expo";
 
 const Page = () => {
-  const { phone } = useLocalSearchParams<{
+  const { phone, isSignIn } = useLocalSearchParams<{
     phone: string;
     isSignIn?: string;
   }>();
+  const { signIn } = useSignIn();
+  const { signUp, setActive } = useSignUp();
+
+  const [verificationCode, setVerificationCode] = useState("");
+
+  useEffect(() => {
+    if (verificationCode.length === 6) {
+      if (isSignIn === "true") {
+        verifySignIn();
+      } else {
+        verifyCode();
+      }
+    }
+  }, [verificationCode]);
+
+  const verifySignIn = async () => {};
+
+  const verifyCode = async () => {};
 
   return (
     <View>
